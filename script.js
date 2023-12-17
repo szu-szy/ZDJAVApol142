7f40828fa2d36137994fe3c27f03ad844bda427e
szy-szu <szy.szumski@gmail.com>
2023-12-17 11:47:16 +0100

    js init

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('dziala!')
+// zadanie 1
+const value = 1;
+// value = 2;
+
+console.log('123')
+
+let firstName = '123';
+firstName = 'nowa wartosc';
+
+console.log(firstName)
+
+// zadanie 3
+const items = [123, 'test', true]
+items.push(null);
+console.log(items)
